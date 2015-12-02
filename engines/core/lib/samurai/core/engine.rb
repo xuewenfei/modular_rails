cbcfbfd16c39ac4de736204688bbc4301cc7ba3d
chongxu <chongxu@b5m.com>
2015-12-02 20:18:35 +0800

    使用devis提供注册信息

--- engines/core/lib/samurai/core/engine.rb
+++ engines/core/lib/samurai/core/engine.rb
@@ -2,6 +2,13 @@ module Samurai
   module Core
     class Engine < ::Rails::Engine
       isolate_namespace Samurai
-    end
+      initializer :append_migrations  do  |app|
+          unless  app.root.to_s.match(root.to_s)
+              config.paths["db/migrate"].expanded.each  do  |p|
+                  app.config.paths["db/migrate"]  <<  p
+              end
+          end
+        end
+     end
   end
 end
